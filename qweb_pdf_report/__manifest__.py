e0d9bcf79186c27482fc5e9722f5846e906eabac
Yatri Modi <yatri1610@gmail.com>
2026-01-07 12:22:29 +0530

    [IMP]

---
+++ qweb_pdf_report/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'QWEB PDF Report',
+    'version': '1.0',
+    'summary': 'QWEB PDF Report',
+    'description': """
+      First DemoModeule Report 
+    """,
+    'depends': ['base','skc_school_management'],
+'data': [
+        'reports/student_profile_card_report.xml',
+
+    ],
+    'author': 'Captivea-india',
+    'website':'www.capti.com',
+    'installable': True,
+    'application': False,
+}
+
+
+
+
+
